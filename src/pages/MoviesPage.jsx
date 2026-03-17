b0bb9dbf053194ac8e9fa5ca12537982b5f88d0d
micheleferri01 <micheleferri095@gmail.com>
2026-03-17 20:37:27 +0100

    update: ui index route page

--- src/pages/MoviesPage.jsx
+++ src/pages/MoviesPage.jsx
@@ -2,9 +2,11 @@ import MoviesList from "../components/MoviesList"
 
 
 export default function MoviesPage() {
-    
+
     return <>
-    <h1 className="text-white py-4 ps-3">Movies</h1>
-    <MoviesList/>
+        <h1 className="text-white py-4 ps-3">Movies</h1>
+        <div className='pb-5'>
+            <MoviesList />
+        </div>
     </>
 }
