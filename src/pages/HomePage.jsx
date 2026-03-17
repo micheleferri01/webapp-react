c07778a4ad5ea31ababc9a6abc84009719f1195d
micheleferri01 <micheleferri095@gmail.com>
2026-03-17 16:29:45 +0100

    add: home page

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,8 @@
+export default function HomePage() {
+    return <>
+        <section className="text-center py-4 text-white">
+            <h1>Welcome</h1>
+            <p>welcome to our site, here you will be able to find all the info of your favorites movies. Have fun!!!</p>
+        </section>
+    </>
+}
