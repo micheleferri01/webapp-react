e5d33066560dc892caaf86f1a3a510009ddbe887
micheleferri01 <micheleferri095@gmail.com>
2026-03-17 20:48:29 +0100

    update: UI card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,11 +4,11 @@ import { Link } from "react-router"
 export default function Card({ movie }) {
     return <>
         <div className="col">
-            <div className="card h-100">
+            <div className="card h-100 bg-black">
                 <div>
                     <img src={movie.image} alt={movie.title} className="film-poster" />
                 </div>
-                <div className="py-3 px-2">
+                <div className="py-3 px-2 text-white">
                     <h1>{movie.title}</h1>
                     <div className="d-flex justify-content-end">
                         <Link to={`/${movie.id}`} className="btn btn-primary">More info</Link>
