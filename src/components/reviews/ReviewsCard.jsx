74b10717e074b000bf4bc6935c54378ff0251866
micheleferri01 <micheleferri095@gmail.com>
2026-03-19 19:51:51 +0100

    add: stars to reviews

--- src/components/reviews/ReviewsCard.jsx
+++ src/components/reviews/ReviewsCard.jsx
@@ -1,7 +1,9 @@
+import Rating from "../Rating";
 
 
 
 export default function ReviewsCard({ review }) {
+   
     return (
         <div className="bg-white py-4 px-2 review-bubble">
             <div className="d-flex gap-2 align-items-center">
@@ -11,7 +13,7 @@ export default function ReviewsCard({ review }) {
                 <p className="m-0">{review.name}</p>
             </div>
             <p>{review.text}</p>
-            <div className="fs-5">{review.vote}<i className="bi bi-star-fill star-color"></i></div>
+            <div className="fs-5"><Rating vote={review.vote}/></div>
         </div>)
 
 }
