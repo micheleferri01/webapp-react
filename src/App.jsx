1bf980fd91702b821db2da133773311675044b23
micheleferri01 <micheleferri095@gmail.com>
2026-03-20 16:49:55 +0100

    add: context provider

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,22 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import DefaultLayout from "./layouts/DefaultLayout";
 import MovieDetailsPage from "./pages/MovieDetailsPage";
 import MoviesPage from "./pages/MoviesPage";
+import { LoadingProvider } from "./contexts/loading";
 
 export default function App() {
 
   return (
     <>
-      <BrowserRouter>
-      <Routes>
-        <Route Component={DefaultLayout}>
-          <Route path='/' Component={MoviesPage}/>
-          <Route path='/:id' Component={MovieDetailsPage}/>
-        </Route>
-      </Routes>
-      </BrowserRouter>
+      <LoadingProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route Component={DefaultLayout}>
+              <Route path='/' Component={MoviesPage} />
+              <Route path='/:id' Component={MovieDetailsPage} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </LoadingProvider>
     </>
   )
 }
