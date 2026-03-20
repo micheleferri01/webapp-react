c03d7f0fb095dbb2dd25d2e62adb0cb55e715ff6
micheleferri01 <micheleferri095@gmail.com>
2026-03-20 18:11:38 +0100

    add: key to the array stars elements

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -4,7 +4,7 @@
 export default function Rating ({vote, maxVote = 5}) {
     const stars = [];
     for (let i = 1; i <= maxVote; i++) {
-        stars.push(<i className={i <= vote ? "bi bi-star-fill star-color" : "bi bi-star star-color"}></i>)
+        stars.push(<i key={i} className={i <= vote ? "bi bi-star-fill star-color" : "bi bi-star star-color"}></i>)
     }
     return stars
 }
